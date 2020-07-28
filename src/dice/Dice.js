cf1791f12c2c004669a71b31f7986ebfce7e656b
Luiz Paulo <lppjunior@gmail.com>
2020-07-27 22:54:41 -0300

    Add parameter validation test

--- src/dice/Dice.js
+++ src/dice/Dice.js
@@ -1,15 +1,21 @@
 class Dice {
-  constructor (faces = 6) {
-    this.faces = faces
+  constructor (sides = 6) {
+    this.valid(sides)
+    this.sides = sides
   }
 
   roll () {
-    this.value = Math.floor((Math.random() * this.faces) + 1)
+    this.value = Math.floor((Math.random() * this.sides) + 1)
     return this
   }
 
   getValue () {
     return this.value
+  }
+
+  valid (sides) {
+    if (typeof sides !== 'number') throw 'The sides number must be a number'
+    if (sides < 1) throw 'The sides number must be greater than zero'
   }
 }
 
--- src/dice/Dices.js
+++ src/dice/Dices.js
@@ -1,12 +1,9 @@
 import Dice from './Dice.js'
 
 class Dices {
-  constructor (amount = null, faces = 6) {
-    if (amount === null) {
-      throw Error('Dice amount is null. Set that! Ex: new Dices(2);')
-    }
-
-    this.dices = Array.from(Array(amount), (_, x) => new Dice(faces))
+  constructor (amount = 2, sides = 6) {
+    this.valid(amount);
+    this.dices = Array.from(Array(amount), (_, x) => new Dice(sides))
   }
 
   roll (options = { unique: false }) {
@@ -19,6 +16,10 @@ class Dices {
     return this
   }
 
+  getAmount () {
+    return this.dices.length
+  }
+
   getValues () {
     return this.dices.map((dice) => dice.getValue())
   }
@@ -28,6 +29,11 @@ class Dices {
       .reduce(
         (unique, item) => (unique.includes(item) ? unique : [...unique, item]), []
       )
+  }
+
+  valid (sides) {
+    if (typeof sides !== 'number') throw 'The sides number must be a number'
+    if (sides < 1) throw 'The sides number must be greater than zero'
   }
 }
 
